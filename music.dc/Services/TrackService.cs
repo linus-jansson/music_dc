bc7df5273b828c1e45872592056c1ae712b0f65e
Linus Jansson <contact@limpan.dev>
2023-03-20 20:33:58 +0100

    Initial commit

---
+++ music.dc/Services/TrackService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// Service to download Tracks in memory and serving them to the discord bot
+namespace music.dc.Services
+{
+	public class YoutubeService
+	{
+		public YoutubeService()
+		{
+		}
+	}
+}
+
